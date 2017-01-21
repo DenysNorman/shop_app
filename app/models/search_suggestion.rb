8d5da658afaa05f1a309123d754dee0029e6a911
Dan Norman <denys.norman.tr.@gmail.com>
2017-01-21 17:48:52 +0200

    add autocomplete

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -1,7 +1,7 @@
 class SearchSuggestion < ApplicationRecord
 
   def self.terms_for(prefix)
-    suggestions = where('term like ?', "#{prefix}_%")
+    suggestions = where('term LIKE ?', "#{prefix}_%")
     suggestions.order('popularity desc').limit(10).pluck(:term)
   end
 
@@ -13,7 +13,7 @@ class SearchSuggestion < ApplicationRecord
   end
 
   def self.index_term(term)
-    where(term: term.downcase).first_or_initialize.tap do |suggestion|
+    where(term: term.downcase).first_or_create.tap do |suggestion|
       suggestion.increment! :popularity
     end
   end
